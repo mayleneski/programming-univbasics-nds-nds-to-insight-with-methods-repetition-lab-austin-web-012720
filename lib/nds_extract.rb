174c7d87d6e99ff91564b8ad8b2429312f39577b
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-12 23:42:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,7 +40,7 @@ def total_gross(directors_database)
   index = 0
 
   while index < directors_database.length do
-    grand_total += directors_totals(directors_database0[list_of_directors(directors_database)[index]]
+    grand_total += directors_totals(directors_database)[list_of_directors(directors_database)[index]]
     index += 1
   end 
   
